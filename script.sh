c6bad278455ffe431f019595d3ab37f5250a8dc5
ahsandev17 <142599778+ahsandev17@users.noreply.github.com>
2023-10-11 13:13:33 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ files=$(ls /var/www/html)
 
 # Create a file called file_names.txt
 touch file_names.txt
-
+touch prob.txt
 # Iterate over the list of files and write each file name to the file_names.txt file
 for file in $files; do
   echo "$file" >> file_names.txt
