4d29948e2627456c2f595b0f197d0e485ecb1479
ahsandev17 <142599778+ahsandev17@users.noreply.github.com>
2023-10-11 16:30:05 +0500

    Update create_file_names.sh

--- .github/scripts/create_file_names.sh
+++ .github/scripts/create_file_names.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-cd /home/ubuntu/
-sudo git clone token@git@github.com:ahsandev17/ResumeCV.git
-
-files=$(ls /var/www/html)
-
-# Create a file called file_names.txt
-touch file_names.txt
 
 
+# files=$(ls /var/www/html)
 
-# Iterate over the list of files and write each file name to the file_names.txt file
-for file in $files; do
-  echo "$file" >> file_names.txt
-done
+# # Create a file called file_names.txt
+# touch file_names.txt
+
+
+
+# # Iterate over the list of files and write each file name to the file_names.txt file
+# for file in $files; do
+#   echo "$file" >> file_names.txt
+# done
